5410e3e7a7d5091171d262324500ffe823682203
JLM <yuanl@ccwcar.com>
2021-03-03 18:34:41 +0800

    fix

---
+++ src/const/crud/mtuser.js
@@ -0,0 +1,35 @@
+export const tableOption = {
+  "border": true,
+  "index": true,
+  "indexLabel": "序号",
+  "stripe": true,
+  "menuAlign": "center",
+  "align": "center",
+  "searchMenuSpan": 6,
+  "column": [
+	  {
+      "type": "input",
+      "label": "",
+      "prop": "id"
+    },	  {
+      "type": "input",
+      "label": "真实姓名",
+      "prop": "realName"
+    },	  {
+      "type": "input",
+      "label": "昵称",
+      "prop": "nickName"
+    },	  {
+      "type": "input",
+      "label": "头像",
+      "prop": "avatar"
+    },	  {
+      "type": "input",
+      "label": "开放Id",
+      "prop": "openId"
+    },	  {
+      "type": "input",
+      "label": "union id",
+      "prop": "unionId"
+    }  ]
+}
